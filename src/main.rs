bbae003b3a73bc38126cc0d16979c322d18b52fe
Dawn Walker <dawniepieuwu@gmail.com>
2024-01-31 17:30:47 +1100

    added .ok()?
    really trying to wrap my head around this whole thing, will need to rewrite it so I know wtf i'm doing

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
             let mut input = String::new();
             std::io::stdin()
                 .read_line(&mut input)
+                .ok()
                 .expect("Failed to read input");
             let val: bool = match input.trim().parse::<i32>() {
                 Ok(num) => num != 0,
